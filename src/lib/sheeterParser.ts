1f6e88e4524a708b78303aa876a11ba5df2fd1f3
Daniel Piotrowski <dpi@leneo.io>
2018-09-21 11:48:17 +0200

    parser

--- src/lib/sheeterParser.ts
+++ src/lib/sheeterParser.ts
@@ -1,23 +1,30 @@
-interface IShParser
-{
+interface IShParser {
     name: string;
     rowsData: any[][];
 }
 
 interface IShOptions {
-    schema: ISheeterOptionsSchema;
+    schema: ISheeterOptionsSchema[];
 }
 
 interface ISheeterOptionsSchema {
-    empty: any;
+    name: string;
+    columns: ISheeterColumn[];
+}
+
+interface ISheeterColumn {
+    name: string;
+    type: string;
+    mandatory: boolean;
 }
 
 export const sheeterParser: (_data: IShParser, _opt: IShOptions) => IShParser  = function(_data: IShParser, _opt: IShOptions): IShParser {
-    for (let index: number = 0; index < _data.rowsData.length; index++) {
+    const headers: string[] = _data.rowsData[0];
+    console.log("@HEADERS: ", headers);
+    for (let index: number = 1; index < _data.rowsData.length; index++) {
         const row: any = _data.rowsData[index];
         for (let rowIndex: number = 0; rowIndex < row.length; rowIndex++) {
-            const rowField: any = row[rowIndex];
-            console.log(rowField);
+            // const rowField: any = row[rowIndex];
         }
     }
     return _data;
